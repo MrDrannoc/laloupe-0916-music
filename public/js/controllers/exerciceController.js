29f580484838d6ef7d62246fb32f347c06d873c7
adrienfloor <adriaandrum@gmail.com>
2017-01-16 20:23:41 +0100

    images size for css transition

--- public/js/controllers/exerciceController.js
+++ public/js/controllers/exerciceController.js
@@ -1,4 +1,4 @@
-function exerciceController(scoreService, noteService, $location, $routeParams) {
+function exerciceController(scoreService, noteService, $location, $routeParams, $timeout) {
     this.scoreService = scoreService;
     this.noteService = noteService;
     this.$location = $location;
@@ -13,14 +13,25 @@ function exerciceController(scoreService, noteService, $location, $routeParams) 
             this.noteCURRENT = [];
             this.numBitBar = this.score.numBitBar;
             this.referenceValueBar = this.score.referenceValueBar;
-            console.log("Toutes les notes présentes sur la première mesure ", this.score.notes);
+            //console.log("Toutes les notes présentes sur la première mesure ", this.score.notes);
 
             // Requete sur la partition pour récupérer les notes la première mesure "score.bars[0]"
 
             for (let note of this.score.notes) {
                 this.noteService.getOne(note._id).then((res) => {
                     this.noteCURRENT.push(res.data);
-                    console.log(this.noteCURRENT);
+                    //console.log(this.noteCURRENT);
+                    if (this.noteCURRENT.length == this.score.notes.length) {
+                        $timeout(() => {
+                            this.mainWidth = 0;
+                            for (note of angular.element('.note')) {
+                                this.mainWidth += note.width;
+                            }
+                            angular.element('.btnPlay').onClick(function(){
+                              
+                            });
+                        }, 0);
+                    }
                 });
             }
 
@@ -30,4 +41,4 @@ function exerciceController(scoreService, noteService, $location, $routeParams) 
     };
     this.load();
 
-  }
+}
