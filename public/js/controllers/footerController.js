b918d99c1976bd511d1eb7e28bad2ff33922e00b
theben67 <benjamin.h.67110@gmail.com>
2017-01-17 10:08:40 +0100

    footer list exo

--- public/js/controllers/footerController.js
+++ public/js/controllers/footerController.js
@@ -1,9 +1,9 @@
 function footerController($rootScope, $location) {
     $rootScope.$on('$locationChangeSuccess', () => {
-        this.hide = $location.$$url.indexOf('/score/editing/') >= 0;
+        this.hide = $location.$$url.indexOf('/score/editing/') >= 0 ? true : $location.$$url.indexOf('/exercice/') >= 0 ? true : false;
     });
     this.load = () => {
-        this.hide = $location.$$url.indexOf('/score/editing/') >= 0;
+        this.hide = $location.$$url.indexOf('/score/editing/') >= 0 ? true : $location.$$url.indexOf('/exercice/') >= 0? true : false;
     };
     this.load();
 }
